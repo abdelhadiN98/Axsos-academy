835da960a18b0268da370799a208722c3adc7812
abdelhadi <abedabednuman12@gmail.com>
2022-01-15 19:55:33 +0200

    initial

---
+++ web fundemental/JavaScript/Button clicker/ButtonClicker.js
@@ -0,0 +1,11 @@
+function Logout(element){
+    element.innerText='Logout';
+}
+
+function alert(element){
+    console.log("ninja was liked");
+}
+
+function remove(element){
+    element.remove();
+}
