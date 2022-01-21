7a1ff81ca8edb3e89b7e2b9ffad345887d3615f8
abdelhadi <abedabednuman12@gmail.com>
2022-01-22 01:31:02 +0200

    initial

--- web fundemental/JavaScript/calculater/calculater.js
+++ web fundemental/JavaScript/calculater/calculater.js
@@ -1,7 +1,48 @@
-var numbers=document.querySelectorAll('[data-number]');
-var operations=document.querySelectorAll('[data-operation]');
-var equal=document.querySelector('[data-equal]');
-var clear=document.querySelector('[data-all-clear]');
-var firstNumber=document.querySelector('[data-firstNumber ]');
-var secondNumber=document.querySelector('[data-secondNumber]');
+var element=document.querySelector("#display")
+var arr=[]
+var newarr1=[]
+var newarr2=[]
 
+var operation=""
+function press(a){
+    if(operation==""){
+    newarr1.push(a)
+    var c=newarr1.join('')
+    element.innerText=c
+}
+else {
+    newarr2.push(a)
+    var o=newarr2.join('')
+    element.innerText=o
+}
+}
+function setOP(operations){
+    var b=document.querySelector("#display")
+    operation=operations
+}
+
+function calculate(){
+    if(operation=="*"){
+        total= parseInt(newarr1.join(''))*parseInt(newarr2.join(''))
+        element.innerText=total
+    }
+    else if(operation=="/"){
+        total=parseInt(newarr1.join(''))/parseInt(newarr2.join(''))
+        element.innerText=total
+    }
+    else if(operation=="+"){
+        total=parseInt(newarr1.join(''))+parseInt(newarr2.join(''))
+        console.log(newarr1,newarr2)
+        element.innerText=total
+    }
+    else if(operation=="-"){
+        total=parseInt(newarr1.join(''))-parseInt(newarr2.join(''))
+        element.innerText=total
+    }
+}
+function clr(){
+    newarr1=[]
+    newarr2=[]
+    operation=""
+    element.innerText=0
+}
