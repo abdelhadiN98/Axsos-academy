14ae91cf9bd7a86809be615eddc92217347192ea
abdelhadi <abedabednuman12@gmail.com>
2022-01-21 16:24:14 +0200

    initial

---
+++ web fundemental/JavaScript/calculater/calculater.js
@@ -0,0 +1,7 @@
+var numbers=document.querySelectorAll('[data-number]');
+var operations=document.querySelectorAll('[data-operation]');
+var equal=document.querySelector('[data-equal]');
+var clear=document.querySelector('[data-all-clear]');
+var firstNumber=document.querySelector('[data-firstNumber ]');
+var secondNumber=document.querySelector('[data-secondNumber]');
+
